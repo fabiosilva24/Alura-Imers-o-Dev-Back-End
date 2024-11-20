fb08a3bc7d981a05bc4c0601a28d77f9637812f6
fabio <155982713+fabiosilva24@users.noreply.github.com>
2024-11-20 21:40:12 +0100

    aula 2

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 import express from 'express';
 
+const posts = [
+    { id: 1, descricao: "Um gatinho curioso explorando o mundo", imagem: "https://placecats.com/200/300"
+    },
+    { id: 2, descricao: "Um gatinho sonolento tomando um cochilo", imagem: "https://placecats.com/300/200?sleepy"
+    },
+    { id: 3, descricao: "Um gatinho brincando com um novelo de lã", imagem: "https://placecats.com/400/300?yarn"
+    },
+    { id: 4,descricao: "Um gatinho com uma expressão muito fofa", imagem: "https://placecats.com/300/300?cute"
+    },
+    { id: 5, descricao: "Um gatinho se escondendo em uma caixa", imagem: "https://placecats.com/200/300?box"
+    },
+  ];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada a noite, com milhares de luzes cintilando, criando um espctaculo magico em Paris.");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => { 
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+
 });
